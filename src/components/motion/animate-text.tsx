d4f1cc19a0b0539d0aa1d5ef4a7ec532fb8513e8
tian7yubao <2079377988@qq.com>
2025-06-21 22:00:22 +0800

    ada animate-text.tsx

---
+++ src/components/motion/animate-text.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import type { Variant, Variants } from "motion/react";
+import React from "react";
+
+import { motion } from "motion/react";
+import { cn } from "@/lib/tailwind";
+
+interface AnimateTextProps {
+    content: string[];
+    as?: keyof React.JSX.IntrinsicElements;
+    stagger?: number;
+    initial: Variant;
+    animate: Variant;
+    className?: string;
+}
+
+export function AnimateText({ as = "p", content, stagger = 0.02, initial, animate, className }: AnimateTextProps) {
+
+    const MotionTag = motion.create(as) as typeof motion.div;
+
+    const container: Variants = {
+        invisible: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: stagger,
+                type: "spring",
+                bounce: 0,
+            }
+        }
+    }
+
+    const item: Variants = {
+        invisible: {
+            ...initial,
+            opacity: 0,
+        },
+        visible: {
+            ...animate,
+            opacity: 1,
+        }
+    }
+
+    return (
+        <MotionTag className={ cn("flex w-fit justify-center flex-wrap", className) }
+                   variants={ container }
+                   initial="invisible"
+                   animate="visible">
+            {
+                content.map((line, idx) => (
+                    <motion.span key={ idx }
+                                 variants={ item }>
+                        { line }
+                    </motion.span>
+                ))
+            }
+        </MotionTag>
+    )
+}
--- src/components/ui/theme/banner/heading.tsx
+++ src/components/ui/theme/banner/heading.tsx
@@ -1,54 +1,33 @@
-"use client"
+import * as motion from "motion/react-client";
 
-import { useEffect, useRef } from "react";
-import { splitText } from "motion-plus";
-import { animate, stagger } from "motion";
+import { AnimateText } from "@/components/motion/animate-text";
 
 export function Heading() {
-
-    const ref = useRef(null);
-
-    useEffect(() => {
-        document.fonts.ready.then(() => {
-            if (ref.current) {
-                const element = ref.current as HTMLDivElement;
-                element.style.visibility = "visible";
-                const { chars } = splitText(element, { splitBy: " ", charClass: "", wordClass: "", lineClass: "" });
-                animate(chars,
-                    {
-                        opacity: [ 0, 1 ],
-                        y: [ 10, 0 ],
-                    },
-                    {
-                        type: "spring",
-                        duration: 2,
-                        bounce: 0,
-                        delay: stagger(0.05),
-                    })
-            }
-        });
-    }, []);
 
     return (
         <section className="py-32 px-5">
             <div className="max-w-7xl mx-auto flex flex-col justify-center items-center gap-y-8">
                 <h1 className="text-center text-5xl md:text-7xl lg:text-9xl">
-                    <span className="text-shadow-lg text-shadow-slate-500/60 invisible **:will-change-transform" ref={ ref }>
-                        Example
-                    </span>
+                    <AnimateText className="text-shadow-lg text-shadow-slate-500/60"
+                                 content={ [ "E", "x", "a", "m", "p", "l", "e" ] }
+                                 initial={ { y: 5 } }
+                                 animate={ { y: 0 } }/>
                 </h1>
                 <h2 className="text-center text-base sm:text-xl md:text-2xl lg:text-3xl">
-                    Each example includes ui & video explanation & pretty source code
+                    <AnimateText className="gap-x-2"
+                                 stagger={ 0.05 }
+                                 content={ [ "Each", "example", "includes", "ui", "&", "video", "explanation", "&", "pretty", "source", "code" ] }
+                                 initial={ { filter: "blur(8px)" } }
+                                 animate={ { filter: "blur(0px)" } }/>
                 </h2>
                 <h3 className="text-center text-4xl md:text-5xl lg:text-6xl">
-                    ctrl + f
+                    <motion.p initial={ { y: 10, opacity: 0 } } animate={ { y: 0, opacity: 1 } }>ctrl + f</motion.p>
                 </h3>
             </div>
         </section>
     )
+
 }
-
-
 
 
 
